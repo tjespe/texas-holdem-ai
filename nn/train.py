b58c178d6cd9987fcea913d74f96028a19461f25
Tord Johan Espe <tordjohanespe@gmail.com>
2024-05-07 13:50:37 +0200

    Split train_nn.py into cv.py and train.py

---
+++ nn/train.py
@@ -0,0 +1,450 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+SELECTED_EPOCHS = 100
+
+# In[3]:
+
+
+import os
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Dropout, Lambda
+from tensorflow.keras.models import Model
+from sklearn.model_selection import train_test_split, KFold
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.regularizers import l1_l2
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.optimizers.legacy import (
+    Adam,
+)  # Using legacy because it runs faster on M1 Macs
+import json
+import re
+import gc
+
+
+# In[4]:
+
+
+stage = "river"
+
+
+# ### Reading and compacting dfs
+
+# In[5]:
+
+
+compact = False
+
+
+# In[6]:
+
+
+# Iterate over files in dfs/*.parquet and combine to one df
+dfs = []
+
+read = []
+for file in os.listdir("dfs"):
+    if file.endswith(".parquet"):
+        read.append(file)
+        df = pd.read_parquet(f"dfs/{file}")
+        dfs.append(df)
+    if file.endswith(".csv"):
+        read.append(file)
+        df = pd.read_csv(f"dfs/{file}", index_col=0)
+        dfs.append(df)
+
+raw_df = pd.concat(dfs, ignore_index=True)
+
+if compact and len(dfs) > 0:
+    print("Compacintg dfs")
+    # Move read files to trash and write combined df to dfs/combined_{timestamp}.parquet
+    trash = "dfs/trash"
+    for f in read:
+        os.rename(f"dfs/{f}", f"{trash}/{f}")
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    raw_df.to_parquet(f"dfs/compacted_{timestamp}.parquet")
+
+dfs = []  # Clear memory
+raw_df
+
+
+# In[7]:
+
+
+# Show all types of columns:
+list(sorted(set(re.sub("\\d+", "X", col) for col in raw_df.columns)))
+
+
+# In[8]:
+
+
+# Filter on stage
+df = raw_df[raw_df["stage"] == stage]
+df = df.drop(columns=["stage", "origin"])
+df
+
+
+# ### Preprocessing
+
+# In[9]:
+
+
+# Divide all bets by game_size
+bet_columns = [
+    "player_bet_in_stage",
+    "player_bet_in_game",
+    "opponent_bet_in_stage",
+    "opponent_bet_in_game",
+    "pot",
+]
+
+
+def scale_bets(df):
+    df[bet_columns] = df[bet_columns].div(df["game_size"], axis=0)
+    return df
+
+
+scale_bets(df)
+df[bet_columns]
+
+
+# In[10]:
+
+
+# Scale range features so that the mean becomes 0 and the standard deviation becomes 1
+range_columns = [col for col in df.columns if col.startswith("prob_")]
+
+
+def scale_ranges(df, training_mean, training_sd):
+    df[range_columns] = (df[range_columns] - training_mean) / training_sd
+    return df
+
+
+def get_training_range_mean_and_sd(df):
+    return df[range_columns].to_numpy().mean(), df[range_columns].to_numpy().std()
+
+
+def revert_range_scaling(df, training_mean, training_sd):
+    df[range_columns] = df[range_columns] * training_sd + training_mean
+    return df
+
+
+mean_training_range_val, sd_training_range_val = get_training_range_mean_and_sd(df)
+scale_ranges(df, mean_training_range_val, sd_training_range_val)
+print("Original mean and sd of range features:")
+print(mean_training_range_val, sd_training_range_val)
+print("Mean and sd of range features after scaling:")
+print(df[range_columns].to_numpy().mean(), df[range_columns].to_numpy().std())
+df[range_columns]
+
+
+# In[11]:
+
+
+# Make boolean features 0 or 1
+bool_columns = [
+    "player_turn",
+    "player_has_bet",
+    "opponent_has_bet",
+]
+
+
+def encode_bools(df):
+    df[bool_columns] = df[bool_columns].astype(int)
+
+
+encode_bools(df)
+df[bool_columns]
+
+
+# In[12]:
+
+
+# Drop game_size column
+df = df.drop(columns=["game_size"])
+df
+
+
+# In[13]:
+
+
+df
+
+
+# In[14]:
+
+
+# Show all types of columns after preprocessing:
+list(sorted(set(re.sub("\\d+", "X", col) for col in df.columns)))
+
+
+# ### Data validation
+
+# In[15]:
+
+
+value_columns = [col for col in df.columns if col.startswith("value_")]
+print("Mean of target variable:", df[value_columns].to_numpy().mean())
+print("SD of target variable:", df[value_columns].to_numpy().std())
+print("Min of target variable:", df[value_columns].to_numpy().min())
+print("Max of target variable:", df[value_columns].to_numpy().max())
+df[value_columns]
+
+
+# In[16]:
+
+
+# Remove any rows with target values outside [-1, 1]
+df = df[(df[value_columns] >= -1).all(axis=1) & (df[value_columns] <= 1).all(axis=1)]
+print("After removing outliers:")
+print("Mean of target variable:", df[value_columns].to_numpy().mean())
+print("SD of target variable:", df[value_columns].to_numpy().std())
+print("Min of target variable:", df[value_columns].to_numpy().min())
+print("Max of target variable:", df[value_columns].to_numpy().max())
+
+
+# In[17]:
+
+
+# Check that values are in correct range
+fig, ax = plt.subplots(figsize=(20, 10))
+plt.scatter(range(df.mean().size), df.mean(), label="means", s=1)
+plt.scatter(range(df.std().size), df.std(), label="stds", s=1)
+plt.legend()
+plt.savefig("means_stds.png")
+
+
+# In[18]:
+
+
+# Plot distribution of target values in a bell curve
+from seaborn import displot
+
+# Sample 1000 rows to speed up plotting
+displot(df[value_columns].sample(1000).to_numpy().flatten(), kind="kde")
+plt.savefig("target_distribution.png")
+
+
+# In[19]:
+
+
+# Check representation of possible public cards
+card_cols = [
+    col for col in df.columns if col.startswith("public_card_") and "*" not in col
+]
+card_counts = df[card_cols].sum()
+fig, ax = plt.subplots(figsize=(20, 10))
+plt.bar(range(len(card_counts)), card_counts)
+plt.savefig("public_card_distribution.png")
+
+
+# In[20]:
+
+
+sub_stages = np.where(
+    df["player_has_bet"] & df["opponent_has_bet"],
+    "respond_to_raise",
+    np.where(df["opponent_has_bet"], "respond", "first_bet"),
+)
+fig, ax = plt.subplots(figsize=(20, 10))
+plt.hist(sub_stages, bins=3)
+plt.title("Sub-stages")
+plt.savefig("sub_stages.png")
+
+
+# In[21]:
+
+
+Y_columns = [col for col in df.columns if col.startswith("value_of_hand_")]
+X_columns = [col for col in df.columns if col not in Y_columns]
+X = df[X_columns].values
+Y = df[Y_columns].values
+print(X.shape, Y.shape)
+
+
+# ### Structure for Neural Network
+
+# In[22]:
+
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+
+
+# In[26]:
+
+
+def generate_model(l1_rate=1e-9, l2_rate=1e-8):
+    # Define input layer
+    input_layer = Input(shape=(X.shape[1],), name="input_layer")
+
+    # Define a small standard deviation for the initial weights
+    init = RandomNormal(mean=0.0, stddev=0.002, seed=42)
+
+    # Only use one hidden layer for now because we have so little data
+    hidden_layer_1 = Dense(
+        1326 * 5,
+        activation="relu",
+        name="hidden_layer_1",
+        kernel_initializer=init,
+        kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate),
+    )(input_layer)
+
+    hidden_layer_2 = Dense(
+        1326 * 5,
+        activation="relu",
+        name="hidden_layer_2",
+        kernel_initializer=init,
+        kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate),
+    )(hidden_layer_1)
+
+    hidden_layer_3 = Dense(
+        1326 * 5,
+        activation="relu",
+        name="hidden_layer_3",
+        kernel_initializer=init,
+        kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate),
+    )(hidden_layer_2)
+
+    # Add a dropout layer to prevent overfitting
+    # dropout_layer = Dropout(0.5)(hidden_layer_3)
+
+    # Define output layer
+    value_layer_P1 = Dense(
+        Y.shape[1],
+        activation="linear",
+        name="value_layer_P1",
+        kernel_initializer=init,
+        kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate),
+    )(hidden_layer_3)
+
+    # Naturally, the target variable is centered around 0 with a SD of ~0.1, thus
+    # to get an SD of 1, it is scaled by 10. To make this scaling implicit in the model,
+    # rather than having to do it in the preprocessing, we add a scaling layer here.
+    scaling_layer = Lambda(lambda x: x * 10)(value_layer_P1)
+
+    # Create an Adam optimizer with a small learning rate
+    small_lr_optimizer = Adam(learning_rate=0.000001)
+
+    # Create model
+    model = Model(inputs=input_layer, outputs=scaling_layer)
+    model.compile(optimizer=small_lr_optimizer, loss="mean_squared_error")
+
+    return model
+
+
+# ### Split data
+
+# In[27]:
+
+
+# Split the data into training and test sets
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=42
+)
+
+
+# %% Training and testing
+
+# Test the model
+model = generate_model()
+epochs = SELECTED_EPOCHS
+model.fit(X_train, Y_train, epochs=epochs)
+test_loss = model.evaluate(X_test, Y_test)
+print(f"Test MSE: {test_loss:.4f}")
+print(f"Test RMSE: {np.sqrt(test_loss):.4f}")
+
+
+# In[33]:
+
+
+# As a baseline, calculate the RMSE of predicting only zeros
+baseline_mse = np.mean(Y_test**2)
+print(f"Baseline MSE: {baseline_mse:.4f}")
+baseline_rmse = np.sqrt(baseline_mse)
+print(f"Baseline RMSE: {baseline_rmse:.4f}")
+
+
+# In[34]:
+
+
+# As a better baseline, calculate the RMSE of predicting the mean of the target variable
+mean_target = np.mean(Y_train, axis=0)
+mean_baseline_mse = np.mean((Y_test - mean_target) ** 2)
+print(f"Mean Baseline MSE: {mean_baseline_mse:.4f}")
+mean_baseline_rmse = np.sqrt(mean_baseline_mse)
+print(f"Mean Baseline RMSE: {mean_baseline_rmse:.4f}")
+
+
+# In[ ]:
+
+
+# Clear memory
+model = None
+gc.collect()
+
+
+# ### Training of final model
+
+# In[ ]:
+
+
+# Train model on all data
+model = generate_model()
+model.fit(X, Y, epochs=epochs)
+
+# Change working directory to models
+os.chdir("models")
+
+# Save the model
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+model_fname = f"model_{stage}_{timestamp}.h5"
+model.save(model_fname)
+
+# Save metadata
+metadata = {
+    "mean_training_range_val": mean_training_range_val,
+    "sd_training_range_val": sd_training_range_val,
+}
+meta_fname = f"model_{stage}_{timestamp}.json"
+with open(meta_fname, "w") as f:
+    json.dump(metadata, f)
+
+# Symlink the model to the latest model
+latest_fname = f"model_{stage}_latest.h5"
+latest_meta_fname = f"model_{stage}_latest.json"
+if os.path.exists(latest_fname):
+    os.remove(latest_fname)
+if os.path.exists(latest_meta_fname):
+    os.remove(latest_meta_fname)
+os.symlink(model_fname, latest_fname)
+os.symlink(meta_fname, latest_meta_fname)
+
+# Change working directory back to parent
+os.chdir("..")
+
+
+# In[ ]:
+
+
+# Load model
+model = tf.keras.models.load_model(f"models/{latest_fname}")
+gc.collect()
+
+
+# In[ ]:
+
+
+# Test making a point prediction
+X_test_point = X_test[0].reshape(1, -1)
+Y_test_point = Y_test[0].reshape(1, -1)
+print("X_test_point shape:", X_test_point.shape)
+Y_pred_point = model.predict(X_test_point)
+print("Test point prediction:")
+print("Actual:", Y_test_point)
+print("Predicted:", Y_pred_point)
