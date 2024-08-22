f9ee14c174ecabcd5539903b61e862b09b85e706
Tord Johan Espe <tordjohanespe@gmail.com>
2024-08-22 14:35:00 +0200

    Give AwareRationalPlayer memory of aggressiveness

--- players/AwareRationalPlayer.py
+++ players/AwareRationalPlayer.py
@@ -8,6 +8,7 @@ from players.RandomPlayer import RandomPlayer
 
 
 def debug_print(*args, **kwargs):
+    return
     print(*args, **kwargs)
 
 
@@ -21,13 +22,22 @@ class AwareRationalPlayer(Player):
     :param alpha: How much the card based winning probability decay as the pot grows (0-1, higher is more decay)
     """
 
-    def __init__(self, name: str = "Rasmus", randomness=0.1, alpha=0.5):
+    def __init__(
+        self, name: str = "Rasmus", randomness=0.1, alpha=0.3, aggression_sensitivity=1
+    ):
         super().__init__()
         self.name = name
         self.raises_per_player = None
         self.implied_winning_probs = None
         self.randomness = randomness
         self.alpha = alpha
+        # Used for registering the number of aggressive actions per player per game
+        # Dimensions: n_games x n_players
+        self.aggression_matrix = None
+        # Used for registering the number of total actions per player
+        # Dimensions: n_games x n_players
+        self.actions_matrix = None
+        self.aggression_sensitivity = aggression_sensitivity
 
     def _ensure_vars_initialized(self, n_players):
         if self.raises_per_player is None:
@@ -35,11 +45,82 @@ class AwareRationalPlayer(Player):
             self.raises_per_player = np.ones(n_players)
         if self.implied_winning_probs is None:
             self.implied_winning_probs = np.full(n_players, fill_value=np.nan)
+        if self.aggression_matrix is None:
+            self.aggression_matrix = np.zeros((1, n_players))
+        if self.actions_matrix is None:
+            self.actions_matrix = np.zeros((1, n_players))
 
     def round_over(self, state: State):
         self.raises_per_player = None
         self.implied_winning_probs = None
         self._ensure_vars_initialized(state.n_players)
+        # Add row of zeros to the action matrices to allow for values for next game
+        self.aggression_matrix = np.vstack(
+            [self.aggression_matrix, np.zeros(state.n_players)]
+        )
+        self.actions_matrix = np.vstack(
+            [self.actions_matrix, np.zeros(state.n_players)]
+        )
+
+    def get_relative_aggression(self, player_i):
+        actions_this_game = self.actions_matrix[-1, player_i]
+        if not actions_this_game:
+            return 1.0
+        aggression_this_game = self.aggression_matrix[-1, player_i] / (
+            actions_this_game
+        )
+        debug_print(f"Calculating relative aggression of player {player_i}")
+        debug_print(
+            f"Total aggression this game: {self.aggression_matrix[-1, player_i]}"
+        )
+        debug_print(f"Total actions this game: {actions_this_game}")
+        debug_print(f"Aggressiveness this game: {aggression_this_game}")
+        aggression_all_games = self.aggression_matrix[:, player_i].sum() / (
+            self.actions_matrix[:, player_i].sum()
+        )
+        debug_print(
+            f"Total aggression all games: {self.aggression_matrix[:, player_i].sum()}"
+        )
+        debug_print(
+            f"Total actions all games: {self.actions_matrix[:, player_i].sum()}"
+        )
+        debug_print(f"Aggressiveness all games: {aggression_all_games}")
+        rel_aggression = aggression_this_game / aggression_all_games
+        debug_print(f"Relative aggression: {rel_aggression}")
+        return rel_aggression
+
+    def get_aggression_index(self, state: State):
+        # Calculate an aggression index based on how aggressive the opponent's have been
+        # in the current game compared to their average aggression
+        relative_aggression_per_player = []
+        for player_i in range(state.n_players):
+            if player_i == state.current_player_i:
+                continue
+            if state.player_is_folded[player_i]:
+                continue
+            relative_aggression_per_player.append(
+                self.get_relative_aggression(player_i)
+            )
+        if not relative_aggression_per_player:
+            return 0
+        return np.mean(relative_aggression_per_player)
+
+    def classify_action(self, state: State, bet: int):
+        """
+        Classify aggression of action based on the size of the bet.
+        Definitions:
+        - Fold: 0
+        - Call: 0.5
+        - Bets: 0.5-1.5 (based on size)
+        """
+        call_bet = max(state.bet_in_stage) - state.bet_in_stage[state.current_player_i]
+        if bet < call_bet:  # i.e. fold
+            return 0
+        if bet == call_bet:
+            return 0.5
+        coef = bet / state.pot
+        debug_print(f"Bet: {bet}, pot: {state.pot}, coef: {coef}")
+        return min(1.5, 0.5 + coef)
 
     def observe_bet(self, from_state: State, bet: int):
         self._ensure_vars_initialized(from_state.n_players)
@@ -53,6 +134,9 @@ class AwareRationalPlayer(Player):
         self.implied_winning_probs[player_i] = max(
             self.implied_winning_probs[player_i], implied_winning_prob
         )
+        aggression = self.classify_action(from_state, bet)
+        self.aggression_matrix[-1, player_i] += aggression
+        self.actions_matrix[-1, player_i] += 1
 
     def get_winning_prob_based_on_raises(self, state: State):
         # Simple model assuming a 1-1 relationship between raises and winning probability
@@ -97,32 +181,39 @@ class AwareRationalPlayer(Player):
             return 0
         current_bet = state.bet_in_stage[current_player_i]
         call_bet = max(state.bet_in_stage) - current_bet
-        card_winning_prob = CheatSheet.get_winning_probability(
+        winning_prob = CheatSheet.get_winning_probability(
             CardCollection(self.hand),
             CardCollection(state.public_cards),
             state.player_is_active.sum(),
         )
+        debug_print(f"Card based winning prob: {winning_prob}")
         # Put less weight on card_winning_prob the more is in the pot
-        pot_factor = state.pot / state.big_blind
-        adjusted_card_winning_prob = card_winning_prob / (
-            1 + self.alpha * np.log(1 + pot_factor)
-        )
+        pot_factor = (state.pot - 2 * state.big_blind) / state.big_blind
+        winning_prob = winning_prob / (1 + self.alpha * np.log(1 + pot_factor))
+        debug_print(f"Adjusted card based winning prob: {winning_prob}")
         raise_based_winning_prob = self.get_winning_prob_based_on_raises(state)
-        winning_prob = np.nanmean(
-            [adjusted_card_winning_prob, raise_based_winning_prob]
+        debug_print(f"Raise based winning prob: {raise_based_winning_prob}")
+        # Only adjust winning prob based on raises if it's lower than card based
+        if raise_based_winning_prob < winning_prob:
+            winning_prob = np.nanmean([winning_prob, raise_based_winning_prob])
+        debug_print(f"Winning prob adjusted for raises: {winning_prob}")
+        aggression_index = self.get_aggression_index(state)
+        over_aggression = max(aggression_index - 1, 0)
+        debug_print(f"Aggression index: {aggression_index}")
+        debug_print(f"Over-aggression: {over_aggression}")
+        # Subtract over-aggression from winning prob
+        winning_prob = max(
+            winning_prob - over_aggression * self.aggression_sensitivity, 0
         )
-        # Cap winning prob at adjusted_card_winning_prob to avoid being fooled
-        winning_prob = min(winning_prob, adjusted_card_winning_prob)
+        debug_print(f"Winning probability adjusted for aggression: {winning_prob}")
         debug_print(
             f"Machine hand: {str(Card(self.hand[0]))} {str(Card(self.hand[1]))}"
         )
-        debug_print(f"Card based winning prob: {card_winning_prob}")
-        debug_print(f"Adjusted card based winning prob: {adjusted_card_winning_prob}")
-        debug_print(f"Raise based winning prob: {raise_based_winning_prob}")
-        debug_print(f"Combined winning prob: {winning_prob}")
         rational_max = winning_prob * state.pot
+        debug_print(f"Rational max: {rational_max}")
         avg_forced_loss = (state.big_blind + state.small_blind) / state.n_players
         rational_max += avg_forced_loss
+        debug_print(f"Rational max adjusted for blinds: {rational_max}")
         if call_bet > rational_max:
             opponent_bluff_chance = self.evaluate_bluff_chance(current_player_i)
             debug_print(f"Opponent bluff chance: {opponent_bluff_chance}")
