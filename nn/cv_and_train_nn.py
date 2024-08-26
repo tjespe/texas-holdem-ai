9b2883fd7b8cd510cd9679477de8fad86a4c4174
Tord Johan Espe <tordjes@stud.ntnu.no>
2024-08-26 11:46:02 +0200

    Remove cv and add cv_and_train_nn

--- nn/cv_and_train_nn.py
+++ nn/cv_and_train_nn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 import os
@@ -17,29 +17,24 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.regularizers import l1_l2
 from tensorflow.keras.initializers import RandomNormal
-from tensorflow.keras.optimizers.legacy import (
-    Adam,
-)  # Using legacy because it runs faster on M1 Macs
+from tensorflow.keras.optimizers.legacy import Adam # Using legacy because it runs faster on M1 Macs
 import json
 import re
 import gc
 
+# In[2]:
 
-# In[4]:
 
-
-stage = "turn"
-
+stage = "river"
 
 # ### Reading and compacting dfs
 
-# In[5]:
+# In[3]:
 
 
 compact = False
 
-
-# In[6]:
+# In[4]:
 
 
 # Iterate over files in dfs/*.parquet and combine to one df
@@ -71,26 +66,24 @@ if compact and len(dfs) > 0:
 dfs = []  # Clear memory
 raw_df
 
-
-# In[7]:
+# In[5]:
 
 
 # Show all types of columns:
 list(sorted(set(re.sub("\\d+", "X", col) for col in raw_df.columns)))
 
-
-# In[8]:
+# In[6]:
 
 
 # Filter on stage
 df = raw_df[raw_df["stage"] == stage]
 df = df.drop(columns=["stage", "origin"])
+print(df.shape)
 df
-
 
 # ### Preprocessing
 
-# In[9]:
+# In[7]:
 
 
 # Divide all bets by game_size
@@ -102,7 +95,6 @@ bet_columns = [
     "pot",
 ]
 
-
 def scale_bets(df):
     df[bet_columns] = df[bet_columns].div(df["game_size"], axis=0)
     return df
@@ -111,27 +103,21 @@ def scale_bets(df):
 scale_bets(df)
 df[bet_columns]
 
-
-# In[10]:
+# In[8]:
 
 
 # Scale range features so that the mean becomes 0 and the standard deviation becomes 1
 range_columns = [col for col in df.columns if col.startswith("prob_")]
-
-
 def scale_ranges(df, training_mean, training_sd):
     df[range_columns] = (df[range_columns] - training_mean) / training_sd
     return df
 
-
 def get_training_range_mean_and_sd(df):
     return df[range_columns].to_numpy().mean(), df[range_columns].to_numpy().std()
-
 
 def revert_range_scaling(df, training_mean, training_sd):
     df[range_columns] = df[range_columns] * training_sd + training_mean
     return df
-
 
 mean_training_range_val, sd_training_range_val = get_training_range_mean_and_sd(df)
 scale_ranges(df, mean_training_range_val, sd_training_range_val)
@@ -141,8 +127,7 @@ print("Mean and sd of range features after scaling:")
 print(df[range_columns].to_numpy().mean(), df[range_columns].to_numpy().std())
 df[range_columns]
 
-
-# In[11]:
+# In[9]:
 
 
 # Make boolean features 0 or 1
@@ -160,31 +145,27 @@ def encode_bools(df):
 encode_bools(df)
 df[bool_columns]
 
-
-# In[12]:
+# In[10]:
 
 
 # Drop game_size column
 df = df.drop(columns=["game_size"])
 df
 
-
-# In[13]:
+# In[11]:
 
 
 df
 
-
-# In[14]:
+# In[12]:
 
 
 # Show all types of columns after preprocessing:
 list(sorted(set(re.sub("\\d+", "X", col) for col in df.columns)))
 
-
 # ### Data validation
 
-# In[15]:
+# In[13]:
 
 
 value_columns = [col for col in df.columns if col.startswith("value_")]
@@ -194,8 +175,7 @@ print("Min of target variable:", df[value_columns].to_numpy().min())
 print("Max of target variable:", df[value_columns].to_numpy().max())
 df[value_columns]
 
-
-# In[16]:
+# In[14]:
 
 
 # Remove any rows with target values outside [-1, 1]
@@ -206,8 +186,7 @@ print("SD of target variable:", df[value_columns].to_numpy().std())
 print("Min of target variable:", df[value_columns].to_numpy().min())
 print("Max of target variable:", df[value_columns].to_numpy().max())
 
-
-# In[17]:
+# In[15]:
 
 
 # Check that values are in correct range
@@ -215,10 +194,10 @@ fig, ax = plt.subplots(figsize=(20, 10))
 plt.scatter(range(df.mean().size), df.mean(), label="means", s=1)
 plt.scatter(range(df.std().size), df.std(), label="stds", s=1)
 plt.legend()
+plt.ylim(top=1.5)
 plt.savefig("means_stds.png")
 
-
-# In[18]:
+# In[16]:
 
 
 # Plot distribution of target values in a bell curve
@@ -228,53 +207,50 @@ from seaborn import displot
 displot(df[value_columns].sample(1000).to_numpy().flatten(), kind="kde")
 plt.savefig("target_distribution.png")
 
-
-# In[19]:
+# In[17]:
 
 
 # Check representation of possible public cards
-card_cols = [
-    col for col in df.columns if col.startswith("public_card_") and "*" not in col
-]
+card_cols = [col for col in df.columns if col.startswith("public_card_") and "*" not in col]
 card_counts = df[card_cols].sum()
 fig, ax = plt.subplots(figsize=(20, 10))
 plt.bar(range(len(card_counts)), card_counts)
 plt.savefig("public_card_distribution.png")
 
-
-# In[20]:
+# In[18]:
 
 
 sub_stages = np.where(
     df["player_has_bet"] & df["opponent_has_bet"],
     "respond_to_raise",
-    np.where(df["opponent_has_bet"], "respond", "first_bet"),
+    np.where(
+        df["opponent_has_bet"],
+        "respond",
+        "first_bet"
+    )
 )
 fig, ax = plt.subplots(figsize=(20, 10))
 plt.hist(sub_stages, bins=3)
 plt.title("Sub-stages")
 plt.savefig("sub_stages.png")
 
+# In[19]:
 
-# In[21]:
 
-
-Y_columns = [col for col in df.columns if col.startswith("value_of_hand_")]
+Y_columns = [col for col in df.columns if col.startswith('value_of_hand_')]
 X_columns = [col for col in df.columns if col not in Y_columns]
 X = df[X_columns].values
 Y = df[Y_columns].values
 print(X.shape, Y.shape)
 
-
 # ### Structure for Neural Network
 
-# In[22]:
+# In[20]:
 
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-
-# In[26]:
+# In[21]:
 
 
 def generate_model(l1_rate=1e-9, l2_rate=1e-8):
@@ -310,7 +286,7 @@ def generate_model(l1_rate=1e-9, l2_rate=1e-8):
     )(hidden_layer_2)
 
     # Add a dropout layer to prevent overfitting
-    dropout_layer = Dropout(0.5)(hidden_layer_3)
+    # dropout_layer = Dropout(0.5)(hidden_layer_3)
 
     # Define output layer
     value_layer_P1 = Dense(
@@ -319,7 +295,7 @@ def generate_model(l1_rate=1e-9, l2_rate=1e-8):
         name="value_layer_P1",
         kernel_initializer=init,
         kernel_regularizer=l1_l2(l1=l1_rate, l2=l2_rate),
-    )(dropout_layer)
+    )(hidden_layer_3)
 
     # Naturally, the target variable is centered around 0 with a SD of ~0.1, thus
     # to get an SD of 1, it is scaled by 10. To make this scaling implicit in the model,
@@ -335,21 +311,17 @@ def generate_model(l1_rate=1e-9, l2_rate=1e-8):
 
     return model
 
-
 # ### Split data
 
-# In[27]:
+# In[22]:
 
 
 # Split the data into training and test sets
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.2, random_state=42
-)
-
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # ### Cross validation
 
-# In[28]:
+# In[23]:
 
 
 # Set up K-Fold cross-validation
@@ -362,10 +334,10 @@ validation_actuals = []
 
 # Define the early stopping callback
 early_stopping = EarlyStopping(
-    monitor="val_loss",  # Monitor the validation loss
-    patience=5,  # Number of epochs with no improvement after which training will be stopped
-    verbose=1,  # To print the message when stopping
-    restore_best_weights=True,  # Restore model weights from the epoch with the best value of the monitored quantity
+    monitor='val_loss',      # Monitor the validation loss
+    patience=5,              # Number of epochs with no improvement after which training will be stopped
+    verbose=1,               # To print the message when stopping
+    restore_best_weights=True  # Restore model weights from the epoch with the best value of the monitored quantity
 )
 
 folds = list(kf.split(X_train))
@@ -377,28 +349,26 @@ for i, (train_index, val_index) in enumerate(folds):
     X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
     Y_train_fold, Y_val_fold = Y_train[train_index], Y_train[val_index]
 
-    print("@@@ FOLD", i + 1, "@@@")
+    print("@@@ FOLD", i+1, "@@@")
     print("Size of training set:", X_train_fold.shape[0])
-
+    
     # Define and compile the neural network inside the loop to reset weights for each fold
     model = generate_model()
-
+    
     # Train the model
     history = model.fit(
-        X_train_fold,
-        Y_train_fold,
+        X_train_fold, Y_train_fold,
         epochs=1000,
         validation_data=(X_val_fold, Y_val_fold),
-        callbacks=[early_stopping],  # Include the early stopping callback here
+        callbacks=[early_stopping]  # Include the early stopping callback here
     )
-
+    
     # Save the history in fold_results
     fold_results.append(history.history)
     validation_predictions.append(model.predict(X_val_fold))
     validation_actuals.append(Y_val_fold)
 
-
-# In[ ]:
+# In[27]:
 
 
 mse_scores = []
@@ -421,22 +391,20 @@ print(f"Cross-Validation RMSE: {rmse:.4f} (+/- {rmse_std_dev:.4f})")
 baseline_rmse = np.sqrt(baseline_mse)
 print(f"Baseline RMSE: {baseline_rmse:.4f}")
 
-
-# In[ ]:
+# In[25]:
 
 
 plt.figure(figsize=(12, 6))
 for i, fold_history in enumerate(fold_results, 1):
-    plt.plot(fold_history["val_loss"], label=f"Fold {i}")
+    plt.plot(fold_history['val_loss'], label=f'Fold {i}')
 
-plt.title("Validation Loss per Fold")
-plt.xlabel("Epochs")
-plt.ylabel("Mean Squared Error (MSE)")
+plt.title('Validation Loss per Fold')
+plt.xlabel('Epochs')
+plt.ylabel('Mean Squared Error (MSE)')
 plt.legend()
-plt.savefig("validation_loss_per_fold.png")
+plt.savefig("val_loss_per_fold.png")
 
-
-# In[ ]:
+# In[34]:
 
 
 # Plot the distribution of outputs from the model
@@ -456,12 +424,13 @@ plt.scatter(
     label="Means of actuals",
     s=1,
     c="red",
-)
+)        
+plt.xlabel("Hand index")
+plt.ylabel("Mean value of hand")
 plt.legend()
-plt.savefig("means_of_predictions_vs_actuals.png")
+plt.savefig("output_target_distribution.png")
 
-
-# In[ ]:
+# In[27]:
 
 
 # Plot the distribution of outputs from the model
@@ -483,17 +452,30 @@ plt.scatter(
     alpha=0.5,
 )
 plt.legend()
-plt.savefig("stds_of_predictions_vs_actuals.png")
+plt.savefig("sd_comparison.png")
+
+# In[36]:
 
 
-# In[ ]:
+# Look at 10 random hands and compare the predicted and actual values
+sample_is = np.random.choice(Y_val.shape[1], 10)
+for i in sample_is:
+    # Plot histogram of actual values
+    fig, ax = plt.subplots(figsize=(20, 10))
+    plt.hist(Y_val[:, i], bins=50, alpha=0.5, label="Actuals")
+    plt.hist(Y_val_pred[:, i], bins=50, alpha=0.5, label="Predictions")
+    plt.legend()
+    plt.title(f"Hand {i}")
+    plt.savefig(f"hand_{i}.png")
+
+# In[28]:
 
 
 # Find optimal number of epochs
 optimal_epochs = []
 
 for result in fold_results:
-    val_loss_per_epoch = result["val_loss"]
+    val_loss_per_epoch = result['val_loss']
     best_epoch = val_loss_per_epoch.index(min(val_loss_per_epoch))
     optimal_epochs.append(best_epoch)
 
@@ -502,3 +484,106 @@ final_epoch_number = int(average_optimal_epoch)
 
 print(f"Suggested number of epochs for final training: {final_epoch_number}")
 
+# In[29]:
+
+
+# Clear memory
+fold_results = []
+gc.collect()
+
+# ### Testing
+
+# In[30]:
+
+
+# Test the model
+model = generate_model()
+epochs = final_epoch_number
+model.fit(X_train, Y_train, epochs=epochs)
+test_loss = model.evaluate(X_test, Y_test)
+print(f"Test MSE: {test_loss:.4f}")
+print(f"Test RMSE: {np.sqrt(test_loss):.4f}")
+
+# In[33]:
+
+
+# As a baseline, calculate the RMSE of predicting only zeros
+baseline_mse = np.mean(Y_test**2)
+print(f"Baseline MSE: {baseline_mse:.4f}")
+baseline_rmse = np.sqrt(baseline_mse)
+print(f"Baseline RMSE: {baseline_rmse:.4f}")
+
+# In[34]:
+
+
+# As a better baseline, calculate the RMSE of predicting the mean of the target variable
+mean_target = np.mean(Y_train, axis=0)
+mean_baseline_mse = np.mean((Y_test - mean_target)**2)
+print(f"Mean Baseline MSE: {mean_baseline_mse:.4f}")
+mean_baseline_rmse = np.sqrt(mean_baseline_mse)
+print(f"Mean Baseline RMSE: {mean_baseline_rmse:.4f}")
+
+# In[ ]:
+
+
+# Clear memory
+model = None
+gc.collect()
+
+# ### Training of final model
+
+# In[ ]:
+
+
+# Train model on all data
+model = generate_model()
+model.fit(X, Y, epochs=epochs)
+
+# Change working directory to models
+os.chdir("models")
+
+# Save the model
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+model_fname = f"model_{stage}_{timestamp}.h5"
+model.save(model_fname)
+
+# Save metadata
+metadata = {
+    "mean_training_range_val": mean_training_range_val,
+    "sd_training_range_val": sd_training_range_val,
+}
+meta_fname = f"model_{stage}_{timestamp}.json"
+with open(meta_fname, "w") as f:
+    json.dump(metadata, f)
+
+# Symlink the model to the latest model
+latest_fname = f"model_{stage}_latest.h5"
+latest_meta_fname = f"model_{stage}_latest.json"
+if os.path.exists(latest_fname):
+    os.remove(latest_fname)
+if os.path.exists(latest_meta_fname):
+    os.remove(latest_meta_fname)
+os.symlink(model_fname, latest_fname)
+os.symlink(meta_fname, latest_meta_fname)
+
+# Change working directory back to parent
+os.chdir("..")
+
+# In[ ]:
+
+
+# Load model
+model = tf.keras.models.load_model(f"models/{latest_fname}")
+gc.collect()
+
+# In[ ]:
+
+
+# Test making a point prediction
+X_test_point = X_test[0].reshape(1, -1)
+Y_test_point = Y_test[0].reshape(1, -1)
+print("X_test_point shape:", X_test_point.shape)
+Y_pred_point = model.predict(X_test_point)
+print("Test point prediction:")
+print("Actual:", Y_test_point)
+print("Predicted:", Y_pred_point)
