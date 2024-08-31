12ee39c98b83388fdfb85ea62ee232dc7056c55e
Tord Johan Espe <tordjohanespe@gmail.com>
2024-08-31 02:18:34 +0200

    Make CheatingPlayer immune againt all in bluff

--- players/CheatingPlayer.py
+++ players/CheatingPlayer.py
@@ -1,8 +1,35 @@
 import numpy as np
-from State import State
 from cpp_poker.cpp_poker import CardCollection, CheatSheet, Oracle
 from PlayerABC import Player
 from helpers import get_random_betting_distribution
+
+
+log_file = open("stats/CheatingPlayer.log", "a")
+
+
+def debug_print(*args, **kwargs):
+    # print(*args, **kwargs)
+    print(*args, **kwargs, file=log_file, flush=True)
+
+
+def combine_probabilities(probs: list[float], player_i: int) -> float:
+    """
+    Combine probabilities of winning for all players except player_i.
+    """
+    # Map each probability into the probability that they win and no opponents
+    # win, then sum them up
+    debug_print("Probs:", probs)
+    mapped_probs = [
+        # Probability of player i winning
+        probs[i]
+        # Probability of no other player winning
+        * np.prod(1 - np.array([p for j, p in enumerate(probs) if i != j]))
+        for i in range(len(probs))
+    ]
+    debug_print("Mapped probs:", mapped_probs)
+    mapped_probs = np.array(mapped_probs) / np.sum(mapped_probs)
+    debug_print("Mapped probs normalized:", mapped_probs)
+    return mapped_probs[player_i]
 
 
 class CheatingPlayer(Player):
@@ -34,25 +61,34 @@ class CheatingPlayer(Player):
             )
             for hand in self.all_hands
         ]
-        print("Own hand:", CardCollection(self.hand).str())
+        debug_print("Own hand:", CardCollection(self.hand).str())
         if state.stage == "river":
             winners = Oracle.find_winner(
                 CardCollection(state.public_cards),
                 [CardCollection(hand) for hand in self.all_hands],
                 state.player_is_active,
             )
-            print("Winners:", winners)
+            debug_print("Winners:", winners)
             winning_prob = (1 if current_player_i in winners else 0) / len(winners)
         else:
-            print("Winner probs:", winner_probs)
-            print("Own winning prob:", winner_probs[current_player_i])
-            print("Sum of winner probs:", sum(winner_probs))
-            winning_prob = winner_probs[current_player_i] / sum(winner_probs)
-        print("Cheating winning prob:", winning_prob)
+            winning_prob = combine_probabilities(winner_probs, current_player_i)
+        debug_print("Cheating winning prob:", winning_prob)
         rational_max = winning_prob * state.pot
-        print("Rational max:", rational_max)
+        debug_print("Rational max:", rational_max)
+        avg_forced_loss = (state.big_blind + state.small_blind) / state.n_players
+        rational_max += avg_forced_loss
+        debug_print("Rational max with forced loss:", rational_max)
         if call_bet > rational_max:
-            print("Folding")
+            # If the other player was irrational, join based on winning chance
+            pot_before_bet = state.pot - call_bet
+            if call_bet > pot_before_bet:
+                debug_print(
+                    "Call bet is higher than pot before bet, evaluating whether to call"
+                )
+                if np.random.rand() < winning_prob:
+                    debug_print("Calling")
+                    return call_bet
+            debug_print("Folding")
             return 0
 
         max_allowed_bet = Oracle.get_max_bet_allowed(
@@ -62,9 +98,9 @@ class CheatingPlayer(Player):
             state.player_piles,
             state.player_is_active,
         )
-        print("Max allowed bet:", max_allowed_bet)
+        debug_print("Max allowed bet:", max_allowed_bet)
         max_bet = min(int(rational_max), max_allowed_bet)
-        print("Max bet:", max_bet)
+        debug_print("Max bet:", max_bet)
 
         # Return random int between call_bet and rational_max
         distribution = get_random_betting_distribution(
@@ -72,8 +108,15 @@ class CheatingPlayer(Player):
             max_bet,
             state.big_blind,
             always_add_fold_chance=False,
-            likelihood_decay=0.2 - 0.15 * len(state.public_cards) / 5,
+            likelihood_decay=0.1 - 0.09 * len(state.public_cards) / 5,
         )
         for i, d in enumerate(distribution):
-            print(f"Bet: {i}, prob: {d}")
+            debug_print(f"Bet: {i}, prob: {d}")
         return np.random.choice(len(distribution), p=distribution)
+
+
+if __name__ == "__main__":
+    print("Testing combine_probabilities")
+    for case in [[0.3, 0.7], [0.45, 0.55], [0.9, 0.5], [0.99, 0.5]]:
+        print("\n\nCase:", case)
+        print("Combined prob:", combine_probabilities(case, 1))
